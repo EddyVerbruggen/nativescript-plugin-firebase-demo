2eefd5015a32b83a5a2dc2d94036d8b8a24953ad
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-11-25 11:22:13 +0100

    Enjoy, world!

---
+++ app/main-view-model.js
@@ -0,0 +1,135 @@
+var observable = require("data/observable");
+var firebase = require("nativescript-firebase");
+var dialogs = require("ui/dialogs");
+var DemoAppModel = (function (_super) {
+  __extends(DemoAppModel, _super);
+  function DemoAppModel() {
+    _super.call(this);
+  }
+
+  DemoAppModel.prototype.doInit = function () {
+    firebase.init({
+      url: 'https://resplendent-fire-4211.firebaseio.com'
+    }).then(
+        function (result) {
+          console.log("firebase.init done");
+        },
+        function (error) {
+          console.log("firebase.init error: " + error);
+        }
+    )
+  };
+
+  DemoAppModel.prototype.doAddChildEventListenerForUsers = function () {
+    var that = this;
+    var onChildEvent = function(result) {
+      that.set("path", '/users');
+      that.set("type", result.type);
+      that.set("key", result.key);
+      that.set("value", JSON.stringify(result.value));
+    };
+
+    firebase.addChildEventListener(onChildEvent, "/users").then(
+        function () {
+          console.log("firebase.addChildEventListener added");
+        },
+        function (error) {
+          console.log("firebase.addChildEventListener error: " + error);
+        }
+    )
+  };
+
+  DemoAppModel.prototype.doAddValueEventListenerForCompanies = function () {
+    var that = this;
+    var onValueEvent = function(result) {
+      that.set("path", '/companies');
+      that.set("type", result.type);
+      that.set("key", result.key);
+      that.set("value", JSON.stringify(result.value));
+    };
+
+    firebase.addValueEventListener(onValueEvent, "/companies").then(
+        function () {
+          console.log("firebase.addValueEventListener added");
+        },
+        function (error) {
+          console.log("firebase.addValueEventListener error: " + error);
+        }
+    )
+  };
+
+  DemoAppModel.prototype.doUserStoreByPush = function () {
+    firebase.push(
+        '/users',
+        {
+          'first': 'Eddy',
+          'last': 'Verbruggen',
+          'birthYear': 1977,
+          'isMale': true,
+          'address': {
+            'street': 'foostreet',
+            'number': 123
+          }
+        }
+    ).then(
+        function () {
+          console.log("firebase.push done");
+        },
+        function (error) {
+          console.log("firebase.push error: " + error);
+        }
+    )
+  };
+
+  DemoAppModel.prototype.doStoreCompaniesBySetValue = function () {
+    firebase.setValue(
+        '/companies',
+
+        // you can store a JSON object
+        //{'foo':'bar'}
+
+        // or even an array of JSON objects
+        [
+          {
+            name: 'Telerik'
+          },
+          {
+            name: 'Google'
+          }
+        ]
+    ).then(
+        function () {
+          console.log("firebase.setValue done");
+        },
+        function (error) {
+          console.log("firebase.setValue error: " + error);
+        }
+    )
+  };
+
+  DemoAppModel.prototype.doRemoveUsers = function () {
+    firebase.remove("/users").then(
+        function () {
+          console.log("firebase.remove done");
+        },
+        function (error) {
+          console.log("firebase.remove error: " + error);
+        }
+    )
+  };
+
+  DemoAppModel.prototype.doRemoveCompanies = function () {
+    firebase.remove("/companies").then(
+        function () {
+          console.log("firebase.remove done");
+        },
+        function (error) {
+          console.log("firebase.remove error: " + error);
+        }
+    )
+  };
+
+  return DemoAppModel;
+})(observable.Observable);
+exports.DemoAppModel = DemoAppModel;
+exports.mainViewModel = new DemoAppModel();
