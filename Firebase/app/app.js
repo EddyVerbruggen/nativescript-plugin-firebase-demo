1f8e6a238d5c16ea790e21e9c301a1e2df2f1108
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-06-25 16:37:28 +0200

    Support for Cloud Messaging (FCM), bump to 3.3.0

--- Firebase/app/app.js
+++ Firebase/app/app.js
@@ -1,36 +1,8 @@
 var application = require("application");
+
+// added this here so we can do some wiring
+var firebase = require("nativescript-plugin-firebase");
+
 application.mainModule = "main-page";
 application.cssFile = "./app.css";
-
-if (application.ios && typeof(FBSDKApplicationDelegate) !== "undefined") {
-
-    var __extends = this.__extends || function (d, b) {
-        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-        function __() { this.constructor = d; }
-        __.prototype = b.prototype;
-        d.prototype = new __();
-    };
-    
-    var appDelegate = (function (_super) {
-        __extends(appDelegate, _super);
-        function appDelegate() {
-            _super.apply(this, arguments);
-        }
-        
-        appDelegate.prototype.applicationDidFinishLaunchingWithOptions = function (application, launchOptions) {
-          FBSDKApplicationDelegate.sharedInstance().applicationDidFinishLaunchingWithOptions(application, launchOptions);
-          return true;
-        };
-        
-        appDelegate.prototype.applicationOpenURLSourceApplicationAnnotation = function (application, url, sourceApplication, annotation) {
-          var handled = FBSDKApplicationDelegate.sharedInstance().applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation);
-          return handled;
-        };
-
-        appDelegate.ObjCProtocols = [UIApplicationDelegate];
-        return appDelegate;
-    })(UIResponder);
-    application.ios.delegate = appDelegate;
-}
-
 application.start();
