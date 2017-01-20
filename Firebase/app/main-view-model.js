2beefd0c39496c597aa9cbbf84e131b7b7035b53
Eddy Verbruggen <eddyverbruggen@gmail.com>
2017-01-20 15:39:32 +0100

    bump to plugin version 3.9.2

--- Firebase/app/main-view-model.js
+++ Firebase/app/main-view-model.js
@@ -102,19 +102,24 @@ var DemoAppModel = (function (_super) {
         // you can use this token to send to your own backend server,
         // so you can send notifications to this specific device
         console.log("Firebase plugin received a push token: " + token);
-        //var pasteboard = utils.ios.getter(UIPasteboard, UIPasteboard.generalPasteboard);
-        //pasteboard.setValueForPasteboardType(token, kUTTypePlainText);
+        // var pasteboard = utils.ios.getter(UIPasteboard, UIPasteboard.generalPasteboard);
+        // pasteboard.setValueForPasteboardType(token, kUTTypePlainText);
       }
     );
     firebase.addOnMessageReceivedCallback(
       function(message) {
+        console.log("----- message received: " + message);
         dialogs.alert({
           title: "Push message: " + (message.title !== undefined ? message.title : ""),
           message: JSON.stringify(message),
           okButtonText: "Sw33t"
         });
       }
-    );
+    ).then(function() {
+      console.log("*********************************** success @ addOnMessageReceivedCallback")
+    }, function (err) {
+      console.log("*********************************** error @ addOnMessageReceivedCallback: " + err)
+    });
   };
 
   DemoAppModel.prototype.doUnregisterForPushNotifications = function () {
